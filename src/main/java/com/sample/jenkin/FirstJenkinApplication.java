13603c0dec9f22136a464fd9e2f7457b73a2def2
Siddeswara Reddy <siddeswarareddy@Siddeswaras-MBP.attlocal.net>
2022-08-19 13:49:26 -0500

    Fourth commit

--- src/main/java/com/sample/jenkin/FirstJenkinApplication.java
+++ src/main/java/com/sample/jenkin/FirstJenkinApplication.java
@@ -14,7 +14,6 @@ public class FirstJenkinApplication {
 		return "Welcome to Jenkins";
 	}
 	
-	
 	public static void main(String[] args) {
 		SpringApplication.run(FirstJenkinApplication.class, args);
 	}
