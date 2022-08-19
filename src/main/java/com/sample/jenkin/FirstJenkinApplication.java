8c1d79cfa7a9e2287a16936d32113bd3b688342c
Siddeswara Reddy <siddeswarareddy@Siddeswaras-MBP.attlocal.net>
2022-08-19 13:42:38 -0500

    second commit

--- src/main/java/com/sample/jenkin/FirstJenkinApplication.java
+++ src/main/java/com/sample/jenkin/FirstJenkinApplication.java
@@ -2,10 +2,18 @@ package com.sample.jenkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class FirstJenkinApplication {
 
+	@GetMapping("/message")
+	public String message() {
+		return "Welcome to Jenkins";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(FirstJenkinApplication.class, args);
 	}
