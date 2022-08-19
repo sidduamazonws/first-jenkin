0357027210ec9b63402342495242eb4789ec7462
Siddeswara Reddy <siddeswarareddy@Siddeswaras-MBP.attlocal.net>
2022-08-19 13:39:16 -0500

    first commit

---
+++ src/main/java/com/sample/jenkin/FirstJenkinApplication.java
@@ -0,0 +1,13 @@
+package com.sample.jenkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstJenkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstJenkinApplication.class, args);
+	}
+
+}
